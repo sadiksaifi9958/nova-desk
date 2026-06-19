d6ae95a79f50e631ccf81b6cdcac7b041afa2df0
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-06-19 15:23:28 +0530

    Add QueryClientProvider for React Query in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AppProvider } from "./context/AppContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <TooltipProvider>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </TooltipProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
